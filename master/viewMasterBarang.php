a5615e790dfcdd32b2ef016062914d409d0f0e47
AndreanMahendra <86004931+AndreanMahendra@users.noreply.github.com>
2021-06-16 21:35:40 +0700

    Upload folder master

---
+++ master/viewMasterBarang.php
@@ -0,0 +1,120 @@
+<?php 
+    $qb = new lsp();
+    $dataMasterBarang = $qb->select("tm_barang_bhp");
+    if ($_SESSION['level'] != "Master") {
+    header("location:../index.php");
+    }
+    if (isset($_GET['delete'])) {
+        $response = $qb->delete("table_barang", "kd_barang", $_GET['id'], "?page=viewBarang");
+    }
+ ?>
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                       <div class="au-breadcrumb-left">
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="#">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item">Data Master Barang BHP</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="main-content" style="margin-top: -60px;">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="au-card-title" style="background-image:url('images/bg-title-01.jpg');">
+                            <div class="bg-overlay bg-overlay--blue"></div>
+                            <h3>
+                            <i class="zmdi zmdi-account-calendar"></i>Data Master Barang BHP</h3>
+                        </div>
+                        <div class="card-body">
+                            <a href="?page=addBarang" class="btn btn-primary"><i class="fa fa-plus"></i> Tambah Barang</a>
+                           </button>
+                           <br>
+                           <br>
+                           <div class="table-responsive">
+                               <table id="example" class="table table-borderless table-striped table-earning">
+                                    <thead>
+                                        <tr>
+                                            <th>ID Barang</th>
+                                            <th>Kategori</th>
+                                            <th>Nama barang</th>
+                                            <th>Harga</th>
+                                            <th>Tanggal Update</th>
+                                            <!-- <th>Harga</th>
+                                            <th>Stok</th>
+                                            <th>Distributor</th> -->
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php 
+                                            $no = 1;
+                                            foreach($dataMasterBarang as $dmb) { 
+                                         ?>
+                                        <tr>
+                                            <td><?= $dmb['id_barang_bhp'] ?></td>
+                                            <td></td>
+                                            <td><?= $dmb['nama_barang_bhp'] ?></td>
+                                            <td><?= $dmb['harga'] ?></td>
+                                            <td><?= $dmb['tanggal_update'] ?></td>
+                                            <!-- <td><?= number_format($dmb['harga_barang']) ?></td>
+                                            <td><?= $dmb['stok_barang'] ?></td>
+                                            <td><?= $dmb['nama_distributor'] ?></td> -->
+                                            <td class="text-center">
+                                                <div class="btn-group">
+                                                    <a href="?page=viewMasterBarangDetail&id=<?php echo $dmb['id_barang_bhp'] ?>" data-toggle="tooltip" data-placement="top" title="Detail" class="btn btn-warning"><i class="fa fa-search"></i></a>
+                                                    <a href="?page=viewMasterBarangEdit&edit&id=<?= $dmb['id_barang_bhp'] ?>" data-toggle="tooltip" data-placement="top" title="Edit" class="btn btn-info"><i class="fa fa-edit"></i></a>
+                                                    <button data-toggle="tooltip" data-placement="top" title="Delete" class="btn btn-danger">
+                                                        <i class="fa fa-trash" id="btdelete<?php echo $no; ?>" ></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <script src="vendor/jquery-3.2.1.min.js"></script>
+                                        <script>
+                                            $('#btdelete<?php echo $no; ?>').click(function(e){
+                                                  e.preventDefault();
+                                                    swal({
+                                                    title: "Hapus",
+                                                    text: "Yakin Hapus?",
+                                                    type: "warning",
+                                                    showCancelButton: true,
+                                                    confirmButtonText: "Yes",
+                                                    cancelButtonText: "Cancel",
+                                                    closeOnConfirm: false,
+                                                    closeOnCancel: true
+                                                  }, function(isConfirm) {
+                                                    if (isConfirm) {
+                                                        window.location.href="?page=viewBarang&delete&id=<?php echo $dmb['kd_barang'] ?>";
+                                                    }
+                                                  });
+                                                });
+                                        </script>
+                                        <?php $no++; } ?>
+                                    </tbody>
+                               </table>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ master/viewRuangan.php
@@ -0,0 +1,170 @@
+<?php 
+    $dis = new lsp();
+    if ($_SESSION['level'] != "Admin") {
+    header("location:../index.php");
+    }
+    $table = "table_ruangan";
+    $dataDis = $dis->select($table);
+    $autokode = $dis->autokode($table,"kd_ruangan","RU");
+
+    if (isset($_GET['delete'])) {
+        $where = "kd_ruangan";
+        $whereValues = $_GET['id'];
+        $redirect = "?page=viewRuangan";
+        $response = $dis->delete($table,$where,$whereValues,$redirect);
+    }
+
+    if (isset($_GET['edit'])) {
+        $id = $_GET['id'];
+        $editData = $dis->selectWhere($table,"kd_ruangan",$id);
+        $autokode = $editData['kd_ruangan'];
+    }
+    if (isset($_POST['getSave'])) {
+        $kd_ruangan   = $dis->validateHtml($_POST['kode_ruangan']);
+        $nama_ruangan = $dis->validateHtml($_POST['nama_ruangan']);
+        
+
+        if ($kd_ruangan == " " || empty($kd_ruangan) || $nama_ruangan == " " || empty($nama_ruangan)) {
+             $response = ['response'=>'negative','alert'=>'Lengkapi field'];
+             }else{
+             $value = "'$kd_ruangan','$nama_ruangan'";
+             $response = $dis->insert($table,$value,"?page=viewRuangan");
+        
+        }
+    }
+
+    if (isset($_POST['getUpdate'])) {
+        $kd_ruangan   = $dis->validateHtml($_POST['kode_ruangan']);
+        $nama_ruangan = $dis->validateHtml($_POST['nama_ruangan']);
+      
+
+        if ($kd_ruangan == "" || $nama_ruangan == "") {
+            $response = ['response'=>'negative','alert'=>'lengkapi field'];
+            }else{
+             $value = "kd_ruangan='$kd_ruangan',nama_ruangan='$nama_ruangan'";
+                    $response = $dis->update($table,$value,"kd_ruangan",$_GET['id'],"?page=viewRuangan");
+        
+            
+        }
+    }
+ ?>
+<section class="au-breadcrumb m-t-75">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="au-breadcrumb-content">
+                       <div class="au-breadcrumb-left">
+                            <ul class="list-unstyled list-inline au-breadcrumb__list">
+                                <li class="list-inline-item active">
+                                    <a href="#">Home</a>
+                                </li>
+                                <li class="list-inline-item seprate">
+                                    <span>/</span>
+                                </li>
+                                <li class="list-inline-item">Data Ruangan</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="main-content" style="margin-top: -60px;">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header" >
+                            <strong class="card-title mb-3">Input Ruangan</strong>
+                        </div>
+                        <div class="card-body">
+                            <form method="post">
+                                <div class="form-group">
+                                    <label for="">Kode Ruangan</label>
+                                    <input type="text" class="form-control form-control-sm" name="kode_ruangan" style="font-weight: bold; color: red;" value="<?php echo $autokode; ?>" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Nama Ruangan</label>
+                                    <input type="text" class="form-control form-control-sm" name="nama_ruangan" value="<?php echo @$editData['nama_ruangan'] ?>">
+                                </div>
+                                
+                                <hr>
+                                <?php if (isset($_GET['edit'])): ?>
+                                <button type="submit" name="getUpdate" class="btn btn-warning"><i class="fa fa-check"></i> Update</button>
+                                <a href="?page=viewRuangan" class="btn btn-danger">Cancel</a>
+                                <?php endif ?>
+                                <?php if (!isset($_GET['edit'])): ?>    
+                                <button type="submit" name="getSave" class="btn btn-primary"><i class="fa fa-download"></i> Simpan</button>
+                                <?php endif ?>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title mb-3">Data Ruangan</strong>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                               <table id="example" class="table table-borderless table-striped table-earning">
+                                   <thead>
+                                       <tr>
+                                            <th>Kode Ruangan</th>
+                                            <th>Nama Ruangan</th>
+                                          
+                                            <th>Action</th>
+                                       </tr>
+                                   </thead>
+                                   <tbody>
+                                        <?php 
+                                            $no = 1;
+                                            foreach($dataDis as $ds){
+                                         ?>
+                                       <tr>
+                                            <td><?= $ds['kd_ruangan'] ?></td>
+                                            <td><?= $ds['nama_ruangan'] ?></td>
+                                           
+                                            <td class="text-center">
+                                                <div class="btn-group">
+                                                    <a data-toggle="tooltip" data-placement="top" title="Edit" href="?page=viewRuangan&edit&id=<?= $ds['kd_ruangan'] ?>" class="btn btn-info"><i class="fa fa-edit"></i></a>
+                                                    <a data-toggle="tooltip" data-placement="top" title="Delete" href="#" class="btn btn-danger"><i class="fa fa-trash" id="btnDelete<?php echo $no; ?>" ></i></a>
+                                                </div>
+                                            </td>
+                                       </tr>
+                                       <script src="vendor/jquery-3.2.1.min.js"></script>
+                                       <script>
+                                        $('#btnDelete<?php echo $no; ?>').click(function(e){
+                                                      e.preventDefault();
+                                                        swal({
+                                                        title: "Hapus",
+                                                        text: "Yakin Ingin menghapus?",
+                                                        type: "error",
+                                                        showCancelButton: true,
+                                                        confirmButtonText: "Yes",
+                                                        cancelButtonText: "Cancel",
+                                                        closeOnConfirm: false,
+                                                        closeOnCancel: true
+                                                      }, function(isConfirm) {
+                                                        if (isConfirm) {
+                                                            window.location.href="?page=viewRuangan&delete&id=<?php echo $ds['kd_ruangan'] ?>";
+                                                        }
+                                                      });
+                                                    });
+                                        </script>
+                                       <?php $no++; } ?>
+                                   </tbody>
+                               </table>
+                           </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
