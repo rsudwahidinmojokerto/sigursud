d4343a55e3af213775742e27c920f776853008a9
AndreanMahendra <andremahendrawork@gmail.com>
2021-07-23 13:35:25 +0700

    struk cetak

--- master/printBhpMasuk.php
+++ master/printBhpMasuk.php
@@ -1,10 +1,16 @@
 <?php 
-	$struk  = new lsp();
-	$id = $_GET['id'];
-	$data   = $struk->edit("transaksi","kd_transaksi",$id);
-	$total  = $struk->selectSumWhere("transaksi","sub_total","kd_transaksi='$id'");
-	$dataDetail = $struk->edit("detailTransaksi","kd_transaksi",$id);
-	$jumlah_barang = $struk->selectSumWhere("transaksi","jumlah","kd_transaksi='$id'");
+	// $struk  = new lsp();
+	// $id = $_GET['id'];
+	// $data   = $struk->edit("transaksi","kd_transaksi",$id);
+	// $total  = $struk->selectSumWhere("transaksi","sub_total","kd_transaksi='$id'");
+	// $dataDetail = $struk->edit("detailTransaksi","kd_transaksi",$id);
+	// $jumlah_barang = $struk->selectSumWhere("transaksi","jumlah","kd_transaksi='$id'");
+$dt = new lsp();
+$status = $_GET['status'];
+$id_transaksi = $_GET['id'];
+
+$detail = $dt->querySelect("SELECT * FROM tr_barang_bhp_" . $status . "_detail LEFT JOIN tm_barang_bhp ON tr_barang_bhp_" . $status . "_detail.id_barang_bhp = tm_barang_bhp.id_barang_bhp LEFT JOIN tm_distributor ON tr_barang_bhp_" . $status . "_detail.id_distributor = tm_distributor.id_distributor WHERE id_transaksi = '" . $id_transaksi . "'");
+$sub = 0;
  ?>
  <style>
  	.col-sm-8{
@@ -40,7 +46,7 @@
             			</div>
             			<div class="card-body">
             				<div class="row">
-				                  <div class="col-sm-6">Kode Transaksi : <?php echo $id ?></div>
+				                  <div class="col-sm-6">Kode Transaksi : <?php echo $id_transaksi ?></div>
 				                  <div class="col-sm-6">
 				                        <p class="text-right"><span><?php echo "Tanggal Cetak : ".date("Y-m-d"); ?></p>
 				                  </div>
@@ -48,35 +54,33 @@
 				            <br>
 				            <table class="table table-striped table-bordered" width="80%">
 								<tr>
-									<td>Kode Antrian</td>
+									<td>ID Daftar</td>
 									<td>Nama Barang</td>
+									<td>Jumlah Stock</td>
 					                <td>Harga Satuan</td>
-									<td>Jumlah</td>
 									<td>Sub Total</td>
 								</tr>
-								<?php foreach ($dataDetail as $dd): ?>
+								<?php 
+								foreach ($detail as $dd): ?>
 								<tr>
-									<td><?= $dd['kd_pretransaksi'] ?></td>
-									<td><?= $dd['nama_barang'] ?></td>
-					                <td><?= $dd['harga_barang'] ?></td>
-									<td><?= $dd['jumlah'] ?></td>
-									<td><?= "Rp.".number_format($dd['sub_total']).",-" ?></td>
+								<td><?= $dd['id_bhp_masuk']; ?></td>
+												<td><?= $dd['nama_barang_bhp']; ?></td>
+												<td><?= number_format($dd['jumlah']); ?></td>
+												<td><?= number_format($dd['harga']); ?></td>
+												<td><?= number_format($dd['harga'] * $dd['jumlah']); ?></td>
 								</tr>
 								<?php endforeach ?>
 								<tr>
-					                <td colspan="2"></td>
-					                <td>Jumlah Pembelian Barang</td>
-					                <td><?php echo $jumlah_barang['sum'] ?></td>
-					                <td></td>
-					            </tr>
-					            <tr>
-									<td colspan="2"></td>
-									<td colspan="2">Total</td>
-									<td><?php echo "Rp.".number_format($total['sum']).",-" ?></td>
+					                <td colspan="1"></td>
+								    <td>Jumlah Pembelian Barang</td>
+					                <td><?= number_format($sub); ?></td>
+					              
+									<td colspan="1">Total</td>
+									<td><?php echo "Rp.".number_format($sub['sum']).",-" ?></td>
 								</tr>
 							</table>
 							<br>
-					            <p>Tanggal Beli : <?php echo $dd['tanggal_beli']; ?></p>
+					            <p>Tanggal Masuk : <?php echo $dd['tanggal_masuk']; ?></p>
 								<br>
 								<a href="#" class="btn btn-info ds" onclick="window.print()"><i class="fa fa-print"></i> Cetak Struk</a>
 								<a href="pageMaster.php?page=viewBhpMasuk" class="btn btn-danger ds">Kembali</a>
